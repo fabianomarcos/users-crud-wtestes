6d273e1a0a0747c2f4c721a76a63f20a7e2f4c02
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-05-29 22:24:32 -0300

    feature: created yup  validations

---
+++ src/ts/utils/validation.ts
@@ -0,0 +1,15 @@
+import * as yup from 'yup'
+
+export const FormValidations = yup.object().shape({
+    name: yup
+      .string()
+      .required('Nome é obrigatório'),
+    email: yup
+      .string()
+      .email("Formato de e-mail inválido!")
+      .required('E-mail é obrigatório'),
+    lastName: yup
+      .string()
+      .min(8, 'Minimum 8 chars')
+      .max(20, 'Max 20 chars')
+})
